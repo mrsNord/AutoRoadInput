5cbb04b2e8f19fe8bd316df774b17e3c42d5eb8b
mrsNord <56041148+mrsNord@users.noreply.github.com>
2021-10-14 09:27:48 -0700

    First release.

--- auto.py
+++ auto.py
@@ -1,44 +1,147 @@
 from typing import List
+from dataclasses import dataclass, field
+import copy
 
-from dataclasses import dataclass
+SegmentCache = []
+LaneCache = []
+
+
+def sanitised_input(prompt, type_=None, min_=None, max_=None, range_=None):
+    if min_ is not None and max_ is not None and max_ < min_:
+        raise ValueError("min_ must be less than or equal to max_.")
+    while True:
+        ui = input(prompt)
+        if type_ is not None:
+            try:
+                ui = type_(ui)
+            except ValueError:
+                print("Input type must be {0}.".format(type_.__name__))
+                continue
+        if max_ is not None and ui > max_:
+            print("Input must be less than or equal to {0}.".format(max_))
+        elif min_ is not None and ui < min_:
+            print("Input must be greater than or equal to {0}.".format(min_))
+        elif range_ is not None and ui not in range_:
+            if isinstance(range_, range):
+                template = "Input must be between {0.start} and {0.stop}."
+                print(template.format(range_))
+            else:
+                template = "Input must be {0}."
+                if len(range_) == 1:
+                    print(template.format(*range_))
+                else:
+                    expected = " or ".join((
+                        ", ".join(str(x) for x in range_[:-1]),
+                        str(range_[-1])
+                    ))
+                    print(template.format(expected))
+        else:
+            return ui
+
 @dataclass
 class Segment:
     '''Object for tracking physical books in a collection.'''
     segmentBegin: float = 0.0
     segmentEnd: float = 0.0
     isMilePost: bool = False
-    pavementType: List[str] = None
+    pavementType: str = ""
 
     def inputData(self):
-        print("Hi Segment")
+        print('')
+        self.segmentBegin = sanitised_input("Segment Begin: ", int) 
+        self.segmentEnd = sanitised_input("Segment End: ", int)
+        tempMile = sanitised_input("Is this a mile post (Y,N)? ", str.lower, range_=['y', 'n'])
+        if tempMile == 'y':
+            self.isMilePost = True
+
+        self.pavementType = sanitised_input("Pavement Type (enter to end)? ", str)
+
+        
+    def processData(self):
+        print("Process")
+        
+    def outputData(self):
+        print("Output")
+
+    def __str__(self):
+        return str(self.segmentBegin) + "," + str(self.segmentEnd) + "," + str(self.isMilePost) + "," + str(self.pavementType)
 
 @dataclass
 class Lane:
     '''Object for tracking physical books in a collection.'''
     laneNumber: int = 0
-    # pavementType[]: str
+    segments: list = field(default_factory=list,init=False)
 
-    segment: List[Segment] = None
+    def __post_init__(self):
+        self.segments = []
 
     def inputData(self):
-        print("Hi Lane " + str(self.laneNumber))
+        print('\nLane ' + str(self.laneNumber))
+
+        anotherLane = 'y'
+        while anotherLane == 'y' or anotherLane == '':
+            if len(SegmentCache) == 0:
+                inputOld = 'n'
+            else:
+                inputOld = sanitised_input("Should we use existing segment [N]? ", str.lower, range_=['y', 'n', ''])
+
+            if inputOld == 'y':
+                print("Segments: Format (Begin,End,isMile,Type)")
+                for i in range(len(SegmentCache)):
+                    print(f'{str(i+1): <{2}}' + "  : " + str(SegmentCache[i]))
+                segmentCopyNum = sanitised_input("What segment should we use (input number)? ", int, 1, len(SegmentCache)+1)
+                tempSegment = copy.deepcopy(SegmentCache[segmentCopyNum-1])
+            else:
+                tempSegment = Segment()
+                tempSegment.inputData()
+                SegmentCache.append(tempSegment)
+            self.segments.append(tempSegment)
+            anotherLane = sanitised_input("Do you have another segment [Y]? ", str.lower, range_=['y', 'n', ''])
+
+    def processData(self):
+        print("Process")
+
+    def outputData(self):
+        print("Output")
+
+    def __str__(self):
+        myStr = ""
+        for x in self.segments:
+            myStr += str(x) + "; "
+
+        return "Lane #" + str(self.laneNumber) + "- (Begin,End,isMile,Type) = " + myStr
 
 @dataclass
 class Highway:
     '''Object for tracking physical books in a collection.'''
     name: str = ""
     direction: str = ""
-    lanes: List[Lane] = None
+    lanes: list = field(default_factory=list,init=False)
     laneCount = 0
 
-    def inputData(self):
-        self.name = input("Highway Name: ")
-        self.direction = input("Direction: (i.e. N, S)")
-        self.laneCount = int(input("Number of Lanes: "))  
+    def __post_init__(self):
         self.lanes = []
+
+    def inputData(self):
+        global LaneCache
+        self.name = sanitised_input("Highway Name: ", str)
+        self.direction = sanitised_input("Direction: (N,S,E,W): ", str.upper, range_=['N', 'S', 'E', 'W'])
+        self.laneCount = sanitised_input("Number of Lanes: ", int, 1)  
         for i in range(self.laneCount):
-            tempLane = Lane(laneNumber=(i+1))
-            tempLane.inputData()
+            if len(LaneCache) == 0:
+                inputOld = 'n'
+            else:
+                inputOld = sanitised_input("Should we use existing lane [N]? ", str.lower, range_=['y', 'n', ''])
+
+            if inputOld == 'y':
+                for j in range(len(LaneCache)):
+                    print(f'{str(j+1): <{2}}' + ": " + str(LaneCache[j]))
+                laneCopyNum = sanitised_input("What lane should we use (input number)? ", int, 1, len(LaneCache)+1)
+                tempLane = copy.deepcopy(LaneCache[laneCopyNum-1])
+            else:
+                tempLane = Lane(laneNumber=(i+1))
+                tempLane.inputData()
+                LaneCache.append(tempLane)
             self.lanes.append(tempLane)
 
     # TODO: Method to do any data processing. IE looking up location on map, finding distances, doing any math...
@@ -60,11 +163,10 @@ class Highway:
     def outputData(self):
         print("Output")
 
-
 # Main code
 highwayIC = Highway()
 highwayIC.inputData()
 
 
 
-print(highwayIC) 
+print(highwayIC)
